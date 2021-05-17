7abaa3c6096eb584c0b56d426df7e5e01e86e534
theoellingsen <83192769+theoellingsen@users.noreply.github.com>
2021-05-17 13:30:39 +1000

    Add project files.

---
+++ Researcher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2SDD
+{
+    public enum Campus { Hobart, Launceston, CradleCoast }
+    class Researcher
+    {
+        public int ID { get; set; }
+
+        public Type Type { get; set; }
+
+        public String GivenName { get; set; }
+
+        public String FamilyName { get; set; }
+
+        public String Title { get; set; }
+
+        public String School { get; set; }
+
+        public String Unit { get; set; }
+
+        public Campus Campus { get; set; }
+
+        public String Email { get; set; }
+
+        public String Photo { get; set; }
+
+        public List<Position> Positions { get; set; }
+
+        public List<Publication> Publications { get; set; }
+
+
+
+        Position CurrentJob(Researcher r)
+        {
+            return r.Positions[0];
+        }
+
+        String CurrentJobTitle(Researcher r)
+        {
+            return r.Positions[0].Title(Positions[0]);
+        }
+
+        DateTime CurrentJobStart(Researcher r)
+        {
+            return r.Positions[0].Start;
+        }
+
+        Position GetEarliestJob(Researcher r)
+        {
+            int length;
+            length = Positions.Count;
+
+            return r.Positions[length];
+        }
+
+        DateTime EarliestStart(Researcher r)
+        {
+            Position p = GetEarliestJob(r);
+            return p.Start;
+        }
+
+        float Tenure(Researcher r)
+        {
+            DateTime tenure = EarliestStart(r);
+            return tenure.CompareTo(DateTime.Now);
+        }
+        int PublicationsCount(Researcher r)
+        {
+            return r.Publications.Count;
+        }
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2SDD
+{
+    //ESSENTIAL
+    class Staff : Researcher
+    {
+        private enum PublicationLevel
+        {
+            A = 5,
+            B = 10,
+            C = 20,
+            D = 32,
+            E = 40,
+        }
+
+        private PublicationLevel Level { get; set; }
+
+        
+        public static float ThreeYearAverage(int ID)
+        {
+           /* // Initiate database object (will be replaced with PublicationController at later date)
+            Database db = new Database();
+
+            // Create list of publications from given researcher
+            List<Publication> publications = new List<Publication>(db.LoadPublications(ID));
+
+            // Select the publications less than three years old
+            var selected = from publications
+                            where (DateTime.Today - selected.Year).Years <= 3
+                            select selected;
+
+            // Create list pf publications from selected
+            List<Publication> lastThree = new List<Publication>(selected);
+            
+            // Return average over three years
+            return lastThree.Count() / 3; */
+        }
+
+        public static float Performance(Researcher r)
+        {
+            // Performance is three year average divided by performance level
+            return ThreeYearAverage(r.ID) /  r.Positions[0].Level * 10;
+        }
+        
+    }
+}
