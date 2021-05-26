e1a1468db3614b8a48e890b700cb13c657194248
David Vonk <djvonk@utas.edu.au>
2021-05-26 13:45:56 +1000

    Added PublicationsController class

---
+++ PublicationsController.cs
@@ -0,0 +1,18 @@
+﻿using A2SDD;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2SDDWPF
+{
+    class PublicationsController
+    {
+        public static ObservableCollection<Publication> LoadPublicationsFor(Researcher r)
+        {
+            return new ObservableCollection<Publication>(Database.LoadPublications(r));
+        }
+    }
+}
