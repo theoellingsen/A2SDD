0e9bf064b2fac9e560d38f0aa33bda426443ecdf
David Humphrey <davidh31@utas.edu.au>
2021-05-27 16:12:03 +1000

    Added more infromation to supervisor

--- Database.cs
+++ Database.cs
@@ -355,7 +355,7 @@ namespace A2SDD
             {
                 conn.Open();
 
-                MySqlCommand cmd = new MySqlCommand("SELECT researcher.degree, staff.given_name, staff.family_name FROM staff, researcher WHERE researcher.supervisor_id = staff.id AND researcher.id=?id", conn);
+                MySqlCommand cmd = new MySqlCommand("SELECT researcher.degree, staff.given_name, staff.family_name, staff.title FROM staff, researcher WHERE researcher.supervisor_id = staff.id AND researcher.id=?id", conn);
 
                 cmd.Parameters.AddWithValue("id", r.ID);
                 rdr = cmd.ExecuteReader();
@@ -365,6 +365,7 @@ namespace A2SDD
                     changed.Degree = rdr.GetString(0);
                     changed.Supervisor.GivenName = rdr.GetString(1);
                     changed.Supervisor.FamilyName = rdr.GetString(2);
+                    changed.Supervisor.Title = rdr.GetString(3);
                 }
 
             }
